bef5a331d7ecda7445710e55dc89ed50e3d46b5d
Jan Redmann <janredmann1@gmail.com>
2021-11-04 17:15:25 +0100

    x

--- game.js
+++ game.js
@@ -27,17 +27,17 @@ class Game {
 		this.playerImage = loadImage('assets/kisspng-european-hedgehog-u0418u0441u0442u043eu0440u0438u0-cartoon-hedgehog-5a7ac1116d4006.8550967915179942574475.png')
 
 		this.backgroundImages = [
-			{ src: loadImage('assets/_11_background.png'), x: 0, speed: 0 },
-			{ src: loadImage('assets/_10_distant_clouds.png'), x: 0, speed: 0.3 },
-			{ src: loadImage('assets/_09_distant_clouds1.png'), x: 0, speed: 0.7 },
-			{ src: loadImage('assets/_08_clouds.png'), x: 0, speed: 1 },
-			{ src: loadImage('assets/_07_huge_clouds.png'), x: 0, speed: 1.3 },
-			{ src: loadImage('assets/_06_hill2.png'), x: 0, speed: 1.7 },
-			{ src: loadImage('assets/_05_hill1.png'), x: 0, speed: 2 },
-			{ src: loadImage('assets/_04_bushes.png'), x: 0, speed: 2.5 },
-			{ src: loadImage('assets/_03_distant_trees.png'), x: 0, speed: 3 },
-			{ src: loadImage('assets/_02_trees and bushes.png'), x: 0, speed: 4 },
-			{ src: loadImage('assets/_01_ground.png'), x: 0, speed: 7 },
+			{ src: loadImage('assets/11background.png'), x: 0, speed: 0 },
+			{ src: loadImage('assets/10distantclouds.png'), x: 0, speed: 0.3 },
+			{ src: loadImage('assets/09distant_clouds1.png'), x: 0, speed: 0.7 },
+			{ src: loadImage('assets/08clouds.png'), x: 0, speed: 1 },
+			{ src: loadImage('assets/07hugeclouds.png'), x: 0, speed: 1.3 },
+			{ src: loadImage('assets/06hill2.png'), x: 0, speed: 1.7 },
+			{ src: loadImage('assets/05hill1.png'), x: 0, speed: 2 },
+			{ src: loadImage('assets/04bushes.png'), x: 0, speed: 2.5 },
+			{ src: loadImage('assets/03distanttrees.png'), x: 0, speed: 3 },
+			{ src: loadImage('assets/02treesandbushes.png'), x: 0, speed: 4 },
+			{ src: loadImage('assets/01ground.png'), x: 0, speed: 7 },
 		]
 
 	}
